381c171d75e4b4a375606ec7ef88d81cd2811ecd
Georgy Varghese <georgy-varghese@users.noreply.github.com>
2024-05-18 16:41:17 +1200

    Use tutorial provided pagination bar

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,20 +1,12 @@
 import { useState } from 'react';
 import JobList from '../components/JobList';
+import PaginationBar from '../components/PaginationBar';
 import { useJobs } from '../lib/graphql/hooks';
 
 const JOBS_PER_PAGE = 10;
 
 function HomePage() {
   const [currentPage, setCurrentPage] = useState(1);
-  const handleClick = (event) => {
-    const id = event.target.id;
-    if (id == "previous") {
-      setCurrentPage(currentPage - 1)
-    }
-    if (id == "next") {
-      setCurrentPage(currentPage + 1)
-    }
-  }
   
   const offset = ((currentPage - 1) * JOBS_PER_PAGE);
   const { loading, error, jobs } = useJobs(JOBS_PER_PAGE, offset);
@@ -34,11 +26,7 @@ function HomePage() {
       <h1 className="title">
         Job Board
       </h1>
-      <div className="buttons has-addons is-centered mt-4">
-          <button id='previous' className="button is-link is-small" onClick={handleClick} disabled={currentPage === 1}>Previous</button>
-          <span className="button is-static is-small">{currentPage} of {totalPages}</span>
-          <button id='next' className="button is-link is-small" onClick={handleClick} disabled={currentPage === totalPages}>Next</button>
-      </div>
+      <PaginationBar currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage}/>
       <JobList jobs={jobList} />
     </div>
   );
